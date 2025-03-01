8a684823bf467133d83e09fcfbda772b80cf2eb6
rahul5400 <rahulp5400@yahoo.com>
2025-03-01 14:49:08 -0500

    pulling colin

--- src/AIResponse.tsx
+++ src/AIResponse.tsx
@@ -1,13 +1,31 @@
-import { Button } from "react-bootstrap";
-import React, { useState } from "react";
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import dotenv from 'dotenv';
+import { useState } from 'react';
 
-let question = "What is the disaster?";
+dotenv.config();
 
-export function Resources(): JSX.Element {
+const apiKey = process.env.API_KEY;
+if (!apiKey) {
+  throw new Error('API_KEY is not defined');
+}
+let hazards = ["Tsunami", "Earthquake", "Flood", "Hurricane/Tornado", "Landslide", "Wildfire", "Drought", "Heatwave", "Blizzard", "Manmade Disaster"];
+
+export async function AIResponse(prompt: string) {
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+    const [answer, setAnswer] = useState<string>("");
+
+    const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompts = "Based on the given hazard, list emergency response actions, preventions, ";
+
+    const result = await model.generateContent(prompts);
+    console.log(result.response.text());
+
     return (
         <div>
-            <h1>{question}</h1>
-            <Button variant="primary">Respond</Button>
+
         </div>
-    );
+    )
 }
+
