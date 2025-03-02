ed1920a298f418b7f2a2ac7fe4f486322a6ea5e5
Colin Parsons <colinparsons5@gmail.com>
2025-03-01 20:41:41 -0500

    CSS looks a lot better now

--- src/App.tsx
+++ src/App.tsx
@@ -29,22 +29,13 @@ function App() {
     setShowPrompt(false);
   };
 
-  //sets the local storage item to the api key the user inputed
-  function handleSubmit() {
-    localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
-  }
-
-  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
-  function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
-    setKey(event.target.value);
-  }
-
   return (
     <Router>
       <div className="App">
         <DisasterPrompt show={showPrompt} onClose={handleDisasterSelect} />
-
+        <div className='title'>
+          <h1>First State Disaster Aid</h1>
+        </div>
         <div className="map-box">
           {disasterType && disasterType !== 'Earthquake' && disasterType !== 'Wildfire' && disasterType !== 'Hurricane' && disasterType !== 'Blizzard' && disasterType !== 'Power Plant Meltdown' && (
             <GoogleMap disasterType={disasterType} />
